7a3d5c1ff83e9bcf0d8b592105f9ebad45b28d4e
Volkan Çakır <66095964+VolkanCakir@users.noreply.github.com>
2022-06-30 17:00:19 +0300

    Update CyriptoApi.java

--- app/src/main/java/com/akilliajans/KriptoTakipv1/services/CyriptoApi.java
+++ app/src/main/java/com/akilliajans/KriptoTakipv1/services/CyriptoApi.java
@@ -9,10 +9,10 @@ import retrofit2.http.GET;
 
 public interface CyriptoApi {
 
-    //https://api.nomics.com/v1/prices?key=085f26fe482d2ebfd856ede279af7ef1b3ff8df0
+    //https://api.nomics.com/v1/prices?key=xx
 
 
-    @GET ("prices?key=085f26fe482d2ebfd856ede279af7ef1b3ff8df0")
+    @GET ("prices?key=xxxx")
     Call<List<CryptoModels>> getModel() ;
 
 }
